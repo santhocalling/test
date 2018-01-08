18ff5b396f4033799051cdd02e2d2a7c3da097db
Santhoshkumar <santhoshkumar.sekar@kbc.be>
2018-01-08 14:34:16 +0100

    first commit

---
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+}
